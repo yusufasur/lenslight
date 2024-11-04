d5b88a7af8284179d9e3b73247c77cd2908f66b1
Yusuf Can Aşur <yusufasur97@gmail.com>
2024-11-04 18:35:38 +0300

    Integrate EJS, restructure project files, update routes

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// esj template engine
+app.set("view engine", "ejs");
+
+// static files middleware
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Index Sayfası");
+  res.render("index");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
