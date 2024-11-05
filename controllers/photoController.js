8c93fa63a68627cc094e6e395f9285172455d8be
Yusuf Can Aşur <yusufasur97@gmail.com>
2024-11-05 22:39:23 +0300

    Add getAPhoto controller method, dynamic link updates, and fix asset paths

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -29,4 +29,17 @@ const getAllPhotos = async (req, res) => {
   }
 };
 
-export { createPhoto, getAllPhotos };
+const getAPhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById(req.params.id);
+
+    res.status(200).render("photo", { photo, currentPage: "photos" });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos, getAPhoto };
--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createPhoto, getAllPhotos } from "../controllers/photoController.js";
+import {
+  createPhoto,
+  getAllPhotos,
+  getAPhoto,
+} from "../controllers/photoController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllPhotos).post(createPhoto);
+router.get("/:id", getAPhoto);
 
 export default router;
